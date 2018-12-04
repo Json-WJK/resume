06b2b752889c907122f78fa62ba5cb5cdc495dde
Json-WJK <44466686+Json-WJK@users.noreply.github.com>
2018-12-04 09:30:03 +0800

    Add files via upload
    脚手架搭建完成，实现功能基本雏形！

---
+++ src/components/Home.vue
@@ -0,0 +1,65 @@
+
+<template>
+    <div>
+        <div v-html="style"></div>
+            <pre v-highlight>
+                <code>
+                    {{march}}
+                </code>
+            </pre>
+    </div>
+</template>
+
+<script>
+    export default{
+        components:{
+
+        },
+        data(){
+            return{
+                march:'',
+                final:
+`
+/*  HR您好！      我叫王俊坤
+    马上过年啦！
+    希望自己能在年前找到一份合适的工作，所以有了这份简历咯!
+    说干就干，我这就开始！ 
+    先给元素加上过渡吧！*/  
+*{
+    transition:.3s;
+}
+    /* 字体调大一点 */                                            
+body{
+    font-size:17px;
+    /* 加个粗 加个背景 加个字体颜色*/
+    
+    background:#002738;
+    color:#eee;
+} 
+`
+            }
+        },
+        methods:{
+            load(){
+                var i=0
+                setInterval(()=>{
+                    this.march=this.final.substring(0,i)
+                    i++
+                },20)
+            }
+        },
+        created(){
+            this.load()
+        },
+        computed:{
+            style(){
+                return '<style>' + this.march + '</style>'
+            }
+        },
+    }
+</script>
+<style>
+.code{
+    background:#002738;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,43 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
+// Vue-cli生成的工程文件的src/main.js
+import hljs from 'highlight.js'
+import 'highlight.js/styles/googlecode.css' //样式文件
+Vue.directive('highlight',el=>{
+  let blocks = el.querySelectorAll('pre code');
+  blocks.forEach((block)=>{
+    hljs.highlightBlock(block)
+  })
+})
+
+Vue.config.productionTip = false
+Vue.use(Vuex)
+var store =new Vuex.Store({
+  state:{
+
+  },
+  mutations:{
+
+  },
+  getters:{
+
+  },
+  actions:{
+
+  }
+})
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
